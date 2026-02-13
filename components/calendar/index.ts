03ec35fecc9f11682beac0e392520bc0b55a7836
Aruldeshwal <144941453+Aruldeshwal@users.noreply.github.com>
2026-02-13 21:45:06 +0530

    Added a lot of functionality

---
+++ components/calendar/index.ts
@@ -0,0 +1,8 @@
+// Components
+export { AvailabilityCalendar } from "./components/availability-calendar";
+export { CalendarToolbar } from "./components/calendar-toolbar";
+export { CopyDayPopover } from "./components/copy-day-popover";
+// Hooks
+export { useCalendarEvents } from "./hooks/use-calendar-events";
+// Types
+export type { SlotInfo, TimeBlock, TimeBlockInteraction } from "./types";
